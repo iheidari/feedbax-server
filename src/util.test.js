c5cbafdfd5a8ef6dd6d65d31d25d11b3e0d53797
iheidari <iman.heydari@gmail.com>
2019-02-21 18:22:04 -0800

    write test for util.compareByProperty

--- src/util.test.js
+++ src/util.test.js
@@ -1,7 +1,43 @@
 var util = require('./util');
+var feedbacks = require('../test/mock').feedbacks;
 
-test('compare', () => {
-  const value = 2 + 2;
-  expect(value).toBeGreaterThan(3);
-  expect(value).toBeGreaterThanOrEqual(3.5);
+describe('compareByProperty', () => {
+  const obj1 = {
+    id: 11,
+    title: 'a',
+    dob: new Date('December 17, 1995 03:24:00')
+  };
+  const obj2 = {
+    id: 35,
+    title: 'A',
+    dob: new Date('December 17, 1995 13:24:00')
+  };
+  const obj3 = {
+    id: 200,
+    title: 'z',
+    dob: new Date('December 17, 1998 03:24:00')
+  };
+
+  test('strings', () => {
+    const compareResult1 = util.compareByProperty('title', 'asc')(obj1, obj2);
+    expect(compareResult1).toBe(0);
+    const compareResult2 = util.compareByProperty('title', 'asc')(obj1, obj3);
+    expect(compareResult2).toBe(-1);
+    const compareResult3 = util.compareByProperty('title', 'desc')(obj2, obj3);
+    expect(compareResult3).toBe(1);
+  });
+
+  test('number', () => {
+    const compareResult1 = util.compareByProperty('id', 'asc')(obj1, obj2);
+    expect(compareResult1).toBe(-1);
+    const compareResult2 = util.compareByProperty('id', 'desc')(obj2, obj3);
+    expect(compareResult2).toBe(1);
+  });
+
+  test('date', () => {
+    const compareResult1 = util.compareByProperty('dob', 'asc')(obj1, obj2);
+    expect(compareResult1).toBe(-1);
+    const compareResult2 = util.compareByProperty('dob', 'desc')(obj2, obj3);
+    expect(compareResult2).toBe(1);
+  });
 });
