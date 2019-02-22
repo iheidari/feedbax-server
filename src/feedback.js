3142d9bba1c868f14e8d9b0a24c54574378c98ca
iheidari <iman.heydari@gmail.com>
2019-02-21 18:20:45 -0800

    create mock data file for test

--- src/feedback.js
+++ src/feedback.js
@@ -1,36 +1,5 @@
 var util = require('./util');
-
-//dummy data for now
-var feedbacks = [
-  {
-    id: 1,
-    title: 'Grid',
-    description:
-      'Material Grid creates visual consistency between layouts while allowing flexibility across a wide variety of designs.'
-  },
-  {
-    id: 2,
-    title: 'Breakpoints',
-    description:
-      'We provide a low-level API that enables the use of breakpoints in a wide variety of contexts.'
-  },
-  {
-    id: 4,
-    title: 'useMediaQuery',
-    description: 'This is a CSS media query hook for React.'
-  },
-  {
-    id: 5,
-    title: 'Hidden',
-    description:
-      'Hidden component can be used to change the visibility of the elements.'
-  },
-  {
-    id: 7,
-    title: 'Sizes',
-    description: 'Fancy larger or smaller buttons? Use the size property.'
-  }
-];
+var feedbacks = require('../test/mock').feedbacks;
 
 function get(req, res, next) {
   var page = req.query.page || 1;
---
+++ test/mock.js
@@ -0,0 +1,32 @@
+module.exports = {
+  feedbacks: [
+    {
+      id: 1,
+      title: 'Grid',
+      description:
+        'Material Grid creates visual consistency between layouts while allowing flexibility across a wide variety of designs.'
+    },
+    {
+      id: 2,
+      title: 'Breakpoints',
+      description:
+        'Provide a low-level API that enables the use of breakpoints in a wide variety of contexts.'
+    },
+    {
+      id: 4,
+      title: 'useMediaQuery',
+      description: 'This is a CSS media query hook for React.'
+    },
+    {
+      id: 5,
+      title: 'Hidden',
+      description:
+        'Hidden component can be used to change the visibility of the elements.'
+    },
+    {
+      id: 7,
+      title: 'Sizes',
+      description: 'Fancy larger or smaller buttons? Use the size property.'
+    }
+  ]
+};
